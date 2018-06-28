7091884dc4696aa78aebe184ab99589c906649af
TigerJ <jimmy.briggs.code@gmail.com>
2018-06-27 21:06:27 -0400

    adding pointer system

--- Assets/Scripts/SpriteStackerEditor.cs
+++ Assets/Scripts/SpriteStackerEditor.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using UnityEditor;
 
 [CustomEditor(typeof(SpriteStacker))]
-public class ObjectBuilderEditor : Editor
+public class SpriteStackerEditor : Editor
 {
     public override void OnInspectorGUI()
     {
